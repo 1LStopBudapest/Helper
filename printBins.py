de0241371569463454b04430c4c59e1aa40df240
Koushik <kmandalforstudy@gmail.com>
2020-08-06 16:25:13 +0200

    mod

--- printBins.py
+++ printBins.py
@@ -3,12 +3,14 @@ import math
 import os, sys
 
 
-from Helper.Binning import *
+from Binning import *
 
-isr = findBinIndex(400, 100, 25)
+isr = findSRBinIndex(401, 100, 30)
 csr = findCTBin(400)
 msr = findMTBin(100)
 lsr = findLepPtBin(26, 100)
 b = findBin(401, 70, 5)
-l = getBinlabel(401, 70, 5, 'CR')
-print isr, csr, msr, lsr, b, l
+l = getBinlabel(401, 70, 5, 'SR')
+l1 = getBinlabel(400, 100, 50, 'CR')
+jsr = findCRBinIndex(401, 100)
+print isr, csr, msr, lsr, b, l, l1, jsr
