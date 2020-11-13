dca4dbc24a25c629b28c800e31b4487622e1556f
RN <rabnora@example.com>
2020-11-13 21:36:48 +0100

    Fixed indentation error in selectJetIdx()

--- TreeVarSel.py
+++ TreeVarSel.py
@@ -146,13 +146,13 @@ class TreeVarSel():
         idx = []
         for j in range(len(self.tr.Jet_pt)):
             if self.tr.Jet_pt[j]>thrsld and abs(self.tr.Jet_eta[j]) < 2.4:
-            clean = True
-            for l in range(len(lepvar)):
-                dR = DeltaR(lepvar[l]['eta'], self.tr.Jet_eta[j], lepvar[l]['phi'], self.tr.Jet_phi[j])
-                ptRatio = float(self.tr.Jet_pt[j])/float(lepvar[l]['pt'])
-                if dR < 0.4 and ptRatio < 2:
-                    clean = False
-                    break
+                clean = True
+                for l in range(len(lepvar)):
+                    dR = DeltaR(lepvar[l]['eta'], self.tr.Jet_eta[j], lepvar[l]['phi'], self.tr.Jet_phi[j])
+                    ptRatio = float(self.tr.Jet_pt[j])/float(lepvar[l]['pt'])
+                    if dR < 0.4 and ptRatio < 2:
+                        clean = False
+                        break
             if clean:
                 idx.append(j)
         return idx
