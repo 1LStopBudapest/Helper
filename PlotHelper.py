f1742e2703aab74f919e7803195891505eac51dd
Armin Kadlecsik <wellington2277@gmail.com>
2021-11-25 14:57:49 +0100

    added ROC plot function

--- PlotHelper.py
+++ PlotHelper.py
@@ -1,5 +1,7 @@
 import ROOT
 import os, sys
+import numpy as np
+import math
 
 from Style import *
 
@@ -126,3 +128,160 @@ def StackHists(files, samplelist, var, dir, cut, islogy=True, scaleOption='Lumis
     hRatioFrame.Draw("HISTsame")
     c.SaveAs(outputdirpath+"/"+var+".png")
     c.Close()
+
+
+# ROC plotting
+def plotROC(signal_pass, signal_total, bk_pass, bk_total, colors, legTitle, path, name, samplename, BKsamplename, xmin = 0, ymin = 0, xmax = 1, ymax = 1, title = "", ptcut = -1, xtitle = "Average signal efficiency", ytitle ="Average BK rejection (1-eff)", cmssimwip = True):
+    # signal_pass, signal_total: nominator and denominator TH1F's, from which signal eff is calculated
+    # bk_pass, bk_total: same for BK rejection
+    # colors: array of colors for each point on ROC
+    # legTitle: array of legend names for points
+    # path, name: file is named <path>/name.png. Leave name empty ("") to auto-format with samplename and BKsamplename
+    # title: the title of the figure. Leave empty ("") to auto-format 
+    # samplename, BKsamplename, ptcut: Auto-formatted uses this name of the signal sample (string), the BK samplename (string), and stating a ptcut (int), if any
+    # xtitle, ytitle: lables of axes
+    # xmin, ymin, xmax, ymax: ranges of axes
+    # cmssimwip: if true, also prints "CMS Simulation Work In Progress" on figure
+
+
+
+    if(len(signal_pass) != len(bk_pass) or len(signal_pass) < len(signal_total) or len(bk_pass) < len(bk_total) or len(colors) < len(signal_pass)):
+        print 'Error in plotROC: arrays are incompatible'
+        return -1
+    
+
+    effs = []
+    Deffs = []
+
+    for i in range(len(signal_pass)):
+        Nbins = signal_pass[i].GetNbinsX()
+        bineffs = []
+        Dbineffs = []
+        for j in range (Nbins):
+            num = signal_pass[i].GetBinContent(j+1)
+            den = signal_total[i].GetBinContent(j+1) 
+            if(den == 0):
+                continue
+            eff = num / den 
+            if(eff > 1):
+                print 'Warning in ROC: eff > 1'
+
+            Dden = signal_total[i].GetBinError(j+1)
+            Dnum = signal_pass[i].GetBinError(j+1)
+            Deff = eff*math.sqrt((Dnum/num)**2 + (Dden/den)**2)
+
+            bineffs.append(eff)
+            Dbineffs.append(Deff)
+        
+        # take average, and take average corrected empirical deviation as errorbars:
+        aveff = sum(bineffs)/len(bineffs)
+        devi = 0
+        for j in range(len(bineffs)):
+            devi += (bineffs[j] - aveff)**2
+        devi /= len(bineffs)*(len(bineffs)-1)
+        devi = math.sqrt(devi)
+
+        effs.append(aveff)
+        Deffs.append(devi)
+
+
+    rejs = []
+    Drejs = []
+
+    for i in range(len(bk_pass)):
+        Nbins = bk_pass[i].GetNbinsX()
+        bineffs = []
+        Dbineffs = []
+        for j in range (Nbins):
+            num = bk_pass[i].GetBinContent(j+1)
+            den = bk_total[i].GetBinContent(j+1) 
+            if(den == 0):
+                continue
+            eff = num / den 
+            if(eff > 1):
+                print 'Warning in ROC: eff > 1'
+
+            Dden = bk_total[i].GetBinError(j+1)
+            Dnum = bk_pass[i].GetBinError(j+1)
+            Deff = eff*math.sqrt((Dnum/num)**2 + (Dden/den)**2)
+
+            bineffs.append(eff)
+            Dbineffs.append(Deff)
+        
+        # take average, and take average corrected empirical deviation as errorbars:
+        aveff = sum(bineffs)/len(bineffs)
+        devi = 0
+        for j in range(len(bineffs)):
+            devi += (bineffs[j] - aveff)**2
+        devi /= len(bineffs)*(len(bineffs)-1)
+        devi = math.sqrt(devi)
+
+        rejs.append(aveff)
+        Drejs.append(devi)
+
+
+    if(title == ""):
+        if(-1 != ptcut):
+            title = "#splitline{ROC for signal: "+samplename+"}{BK: "+BKsamplename+", pt > "+str(ptcut)+" GeV}"
+        else:
+            title = "#splitline{ROC for signal: "+samplename+"}{BK: "+BKsamplename+"}"
+
+    roc = {}
+    for i in range(len(effs)):
+        x = [effs[i]]
+        y = [rejs[i]]
+        Dx = [Deffs[i]]
+        Dy = [Drejs[i]]
+        roc[i] = ROOT.TGraphAsymmErrors(1,np.array(x),np.array(y),np.array(Dx),np.array(Dx),np.array(Dy),np.array(Dy))
+    
+        roc[i].SetLineColor(colors[i])
+        roc[i].SetLineWidth(2)
+        roc[i].SetMarkerSize(0.8)
+        roc[i].SetMarkerStyle(20)
+        roc[i].SetMarkerColor(colors[i])
+        roc[i].SetTitle(title)
+
+    leg = ROOT.TLegend(0.5, 0.1, 0.9, 0.3)
+    for i in range(len(roc)):    
+        leg.AddEntry(roc[i], legTitle[i] ,"p")
+
+    c = ROOT.TCanvas('c', 'Title', 600, 800)
+
+    c.SetGrid()
+    fr = c.DrawFrame(xmin,ymin,xmax,ymax)
+
+    fr.SetTitle(title)
+    fr.SetTitleSize(0.01)
+
+    fr.GetYaxis().SetTitle(ytitle)
+    fr.GetYaxis().SetTitleSize(0.04)
+    fr.GetYaxis().SetTitleOffset(1.15)
+    fr.GetYaxis().SetLabelSize(0.03)
+    fr.GetXaxis().SetTitle(xtitle)
+    fr.GetXaxis().SetTitleSize(0.04)
+    fr.GetXaxis().SetTitleOffset(0.9)
+    fr.GetXaxis().SetLabelSize(0.03)
+
+
+
+    if(cmssimwip):
+        tex = ROOT.TLatex(0.43,0.875,"CMS Simulation work in progress")
+        tex.SetNDC()
+        tex.SetTextFont(42)
+        tex.SetTextSize(0.035)
+        tex.SetLineWidth(2)
+        tex.Draw()
+
+    for i in range(len(roc)):
+        roc[i].Draw("P,SAME")
+    leg.Draw("SAME")
+
+
+    if(name == ""):
+        name = "ROCfigure_Sig_"+samplename+"_BK_"+BKsamplename
+        if(ptcut != -1):
+            name += "_pt"+str(ptcut)
+
+    c.SaveAs(path+"/"+name+".png")
+    c.Close()
+    return 0
