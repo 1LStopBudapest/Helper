9a67c2d7b0a87dfd06f6d425e9fbb75c3b892500
RN <rabnora@example.com>
2022-05-23 15:14:43 +0200

    comparison of closest elements only

--- TreeVarSel_true.py
+++ TreeVarSel_true.py
@@ -34,9 +34,23 @@ class TreeVarSel_true():
     def getLSP(self):
         L = []
         for i in range(self.tr.nGenPart):
-            if abs(self.tr.GenPart_pdgId[i]) == 1000022: # \tilde{chi}_1^0 == 1000022
+            if self.tr.GenPart_pdgId[i] == 1000022 and abs(self.tr.GenPart_pdgId[self.tr.GenPart_genPartIdxMother[i]]) == 1000006: # \tilde{chi}_1^0 == 1000022
                 L.append({'x':self.tr.GenPart_vx[i], 'y':self.tr.GenPart_vy[i], 'z':self.tr.GenPart_vz[i]})
         return L #always 2 elements
+
+    def getLSP_S(self): #from stop
+        L = {}
+        for i in range(self.tr.nGenPart):
+            if self.tr.GenPart_pdgId[i] == 1000022 and self.tr.GenPart_pdgId[self.tr.GenPart_genPartIdxMother[i]] == 1000006:
+                L = {'x':self.tr.GenPart_vx[i], 'y':self.tr.GenPart_vy[i], 'z':self.tr.GenPart_vz[i]}
+        return L 
+
+    def getLSP_A(self): #from antistop
+        L = {}
+        for i in range(self.tr.nGenPart):
+            if self.tr.GenPart_pdgId[i] == 1000022 and self.tr.GenPart_pdgId[self.tr.GenPart_genPartIdxMother[i]] == -1000006:
+                L = {'x':self.tr.GenPart_vx[i], 'y':self.tr.GenPart_vy[i], 'z':self.tr.GenPart_vz[i]}
+        return L
 
     def getPV(self):
         return {'x':self.tr.PV_x, 'y':self.tr.PV_y, 'z':self.tr.PV_z}
@@ -44,16 +58,32 @@ class TreeVarSel_true():
     #deprecated
     def getSV(self):
         L = []
-        for i in range(self.tr.nSV):
+        for i in self.selectSoftBIdx():
             L.append({'x':self.tr.SV_x[i], 'y':self.tr.SV_y[i], 'z':self.tr.SV_z[i]})
         return L
 
-    def genB(self):
+    def getB(self):
         L = []
         for i in range(self.tr.nGenPart):
             if abs(self.tr.GenPart_pdgId[i]) == 5 and self.tr.GenPart_genPartIdxMother[i] >=0 and self.tr.GenPart_genPartIdxMother[i]<self.tr.nGenPart:
                 if abs(self.tr.GenPart_pdgId[self.tr.GenPart_genPartIdxMother[i]])==1000006:
                     L.append({'x':self.tr.GenPart_vx[i], 'y':self.tr.GenPart_vy[i], 'z':self.tr.GenPart_vz[i]})
+        return L
+
+    def getB_S(self):
+        L = {}
+        for i in range(self.tr.nGenPart):
+            if self.tr.GenPart_pdgId[i] == 5 and self.tr.GenPart_genPartIdxMother[i] >=0 and self.tr.GenPart_genPartIdxMother[i]<self.tr.nGenPart:
+                if self.tr.GenPart_pdgId[self.tr.GenPart_genPartIdxMother[i]] == 1000006:
+                    L = {'x':self.tr.GenPart_vx[i], 'y':self.tr.GenPart_vy[i], 'z':self.tr.GenPart_vz[i]}
+        return L
+
+    def getB_A(self):
+        L = {}
+        for i in range(self.tr.nGenPart):
+            if self.tr.GenPart_pdgId[i] == -5 and self.tr.GenPart_genPartIdxMother[i] >=0 and self.tr.GenPart_genPartIdxMother[i]<self.tr.nGenPart:
+                if self.tr.GenPart_pdgId[self.tr.GenPart_genPartIdxMother[i]] == -1000006:
+                    L = {'x':self.tr.GenPart_vx[i], 'y':self.tr.GenPart_vy[i], 'z':self.tr.GenPart_vz[i]}
         return L
 
     def distance(self, v1, v2, coord):
@@ -64,4 +94,10 @@ class TreeVarSel_true():
         for l1 in L1:
             for l2 in L2:
                 D.append(abs(l1[coord]-l2[coord]))
+        return D
+
+    def smallestDist(self, v, L, coord):
+        D = 1e5
+        for l in L:
+            D = min(D, abs(l[coord]-v[coord]))
         return D
