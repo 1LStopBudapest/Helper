12fed93ee73c7ba1d6813848d5c24f66d0a02959
RN <rabnora@example.com>
2022-07-05 22:47:27 +0200

    new functions: smallestDist*

--- TreeVarSel_true.py
+++ TreeVarSel_true.py
@@ -21,6 +21,13 @@ class TreeVarSel_true():
                 genStop.append({'x':self.tr.GenPart_vx[i], 'y':self.tr.GenPart_vy[i], 'z':self.tr.GenPart_vz[i]})
         return genStop[0] #this list always has only 1 element
 
+    def get106(self):
+        genStop = {}
+        for i in range(self.tr.nGenPart):
+            if self.tr.GenPart_pdgId[i]==1000006 and self.tr.GenPart_status[i]==106:
+                genStop = {'x':self.tr.GenPart_vx[i], 'y':self.tr.GenPart_vy[i], 'z':self.tr.GenPart_vz[i]}
+        return genStop
+
     def getGenPartAntiStop(self):
         genAStop = []
         for i in range(self.tr.nGenPart):
@@ -66,13 +73,6 @@ class TreeVarSel_true():
     def getPV(self):
         return {'x':self.tr.PV_x, 'y':self.tr.PV_y, 'z':self.tr.PV_z}
 
-    #deprecated
-    def getSV(self):
-        L = []
-        for i in self.selectSoftBIdx():
-            L.append({'x':self.tr.SV_x[i], 'y':self.tr.SV_y[i], 'z':self.tr.SV_z[i]})
-        return L
-
     def getB(self):
         L = []
         for i in range(self.tr.nGenPart):
@@ -98,15 +98,20 @@ class TreeVarSel_true():
     def distance(self, v1, v2, coord):
         return abs(v1[coord]-v2[coord])
 
-    def listDist(self, L1, L2, coord):
-        D = []
-        for l1 in L1:
-            for l2 in L2:
-                D.append(abs(l1[coord]-l2[coord]))
-        return D
-
-    def smallestDist(self, v, L, coord):
+    def smallestDist(self, v, L, coord): #v: vector, L: list of vectors
         D = 1e5
         for l in L:
             D = min(D, abs(l[coord]-v[coord]))
+        return D
+
+    def smallestDist2D(self, v, L):
+        D = 1e5
+        for l in L:
+            D = min(D, sqrt((v['x'] - l['x'])**2 + (v['y'] - l['y'])**2))
+        return D
+
+    def smallestDist3D(self, v, L):
+        D = 1e5
+        for l in L:
+            D = min(D, sqrt((v['x'] - l['x'])**2 + (v['y'] - l['y'])**2 + (v['z'] - l['z'])**2))            
         return D
